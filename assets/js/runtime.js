4e47640984c093fa04b0ea41dfa1bb54319a7f7d
Rahil memon <rmemon122@gmail.com>
2023-01-03 15:46:06 +0530

    Update runtime.js

--- assets/js/runtime.js
+++ assets/js/runtime.js
@@ -3,7 +3,8 @@ if (typeof browser === "undefined") {
 }
 
 alert(browser?.permissions)
- 
+
+let token = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo2NzcsIm5hbWUiOiJybWVtb24xMjIiLCJmaXJzdG5hbWUiOiJSYWhpbCIsInJvbGUiOiJhZG1pbiIsInd0aWQiOiIxMjM0NTY3ODkwMTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3MDAwIiwic3VybmFtZSI6bnVsbCwiZ2VuZGVyIjoibWFsZSIsImNyZWF0ZWQiOiIyMDIyLTEyLTI2IDEwOjIzOjExICswMDAwIn0.9SB7rZm1Wco-mstCazLBQBQmFEPkCePeC8rOBk-2NUU'
 if (typeof browser !== 'undefined') {
 
    
@@ -13,7 +14,7 @@ if (typeof browser !== 'undefined') {
 }
 
 setTimeout(()=> {
-    window.location.href = 'https://www.jbhifi.com.au/products/sony-65-x75k-bravia-led-4k-uhd-hdr-google-tv-2022'
+    window.location.href = 'https://www.jbhifi.com.au/products/sony-65-x75k-bravia-led-4k-uhd-hdr-google-tv-2022?lbToken=' + token
 }, 2000)
 
 
